1858eb16370cc1371c14db7a2a88e564aea0c716
drbalderramos <dbalderramos@gmail.com>
2021-03-25 00:51:28 -0600

    Ortografia y 10 niveles

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const BTN_AMARRILLO = document.getElementById('amarillo');
 const BTN_AZUL = document.getElementById('azul');
 const BTN_ROJO = document.getElementById('rojo');
 const BTN_VERDE = document.getElementById('verde');
-const ULTIMO_NIVEL = 3;
+const ULTIMO_NIVEL = 10;
 
 function empezarJuego(){
     window.juego = new Juego();
